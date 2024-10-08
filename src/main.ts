519e078ac24681a87a723035e1c8a795e78e4225
Justin Xu <jxu240@ucsc.edu>
2024-10-08 00:27:45 -0700

    Step 1 complete

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ const app: HTMLDivElement = document.querySelector("#app")!;
 const gameName = "Justin Xu's Game";
 document.title = gameName;
 
+const button = document.createElement("button");
+button.innerHTML = "😼";
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+app.append(button)
