0cef2ea39833497a5bc3c2c527548e42bb2630ab
Justin Xu <jxu240@ucsc.edu>
2024-10-08 00:57:46 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,17 @@ document.title = gameName;
 const button = document.createElement("button");
 button.innerHTML = "😼";
 
+let counter: number = 0;
+const newDiv = document.createElement("div");
+button.addEventListener("click", () => {
+    counter += 1
+    newDiv.innerHTML = `${counter} smirks 😼`
+});
+newDiv.innerHTML = `${counter} smirks 😼`
+
+
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
-app.append(button)
+app.append(button);
+app.append(newDiv);
